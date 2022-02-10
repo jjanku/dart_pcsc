1749f262a9335980c775ac14eb85c1a7e822d981
Jakub Janků <janku.jakub.jj@gmail.com>
2022-02-10 18:18:15 +0100

    open pcsc lib

--- lib/src/dart_pcsc_base.dart
+++ lib/src/dart_pcsc_base.dart
@@ -1,1 +1,13 @@
+import 'dart:ffi';
+import 'dart:io';
 
+import 'generated/pcsc_lib.dart';
+
+String _pcscLibName() {
+  if (Platform.isLinux) return 'libpcsclite.so.1';
+  throw UnsupportedError('Platform unsupported');
+}
+
+PcscLib _pcscLibOpen() => PcscLib(DynamicLibrary.open(_pcscLibName()));
+
+late final _pcscLib = _pcscLibOpen();

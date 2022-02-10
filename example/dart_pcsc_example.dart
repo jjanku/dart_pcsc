97a6e9d15588c0d5efcbd79c6de5c7dc90d0d902
Jakub Janků <janku.jakub.jj@gmail.com>
2022-02-10 12:36:08 +0100

    add auto-generated files

---
+++ example/dart_pcsc_example.dart
@@ -0,0 +1,6 @@
+import 'package:dart_pcsc/dart_pcsc.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/dart_pcsc.dart
@@ -0,0 +1,8 @@
+/// Support for doing something awesome.
+///
+/// More dartdocs go here.
+library dart_pcsc;
+
+export 'src/dart_pcsc_base.dart';
+
+// TODO: Export any libraries intended for clients of this package.

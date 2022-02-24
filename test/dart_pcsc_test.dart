9b4297bbe9bd119062825dc3454d74ab98214361
Jakub Janků <janku.jakub.jj@gmail.com>
2022-02-24 09:06:22 +0100

    WIP: test

--- test/dart_pcsc_test.dart
+++ test/dart_pcsc_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:dart_pcsc/dart_pcsc.dart';
 import 'package:test/test.dart';
 
@@ -10,7 +12,17 @@ void main() {
     test('First Test', () async {
       final ctx = PcscContext();
       await ctx.establish(Scope.user);
+      final cancellable = ctx.waitForReaderChange();
+      await Future.delayed(Duration(seconds: 2));
+      // cancellable.cancel();
       print(await ctx.listReaders());
+      // final readers = await ctx.listReaders();
+      // print(readers);
+      // final ready = await ctx.waitForCard(readers).value;
+      // print(ready);
+      // final card = await ctx.connect(ready[0], ShareMode.shared, Protocol.any);
+      // final resp = await card.transmit(Uint8List.fromList([1, 2, 3, 4, 5, 6]));
+      // print(resp);
       await ctx.release();
     });
   });

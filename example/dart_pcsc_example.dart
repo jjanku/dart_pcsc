83162314aa81e707748d45d0263b15b4b541b458
Jakub Janků <janku.jakub.jj@gmail.com>
2022-03-06 18:59:44 +0100

    add example

--- example/dart_pcsc_example.dart
+++ example/dart_pcsc_example.dart
@@ -1,3 +1,39 @@
+import 'dart:typed_data';
+
 import 'package:dart_pcsc/dart_pcsc.dart';
 
-void main() {}
+void main() async {
+  final context = PcscContext();
+  try {
+    await context.establish(Scope.user);
+
+    List<String> readers = await context.listReaders();
+
+    if (readers.isEmpty) {
+      print('No readers');
+      return;
+    }
+    print('Readers: $readers');
+
+    print('Waiting for card');
+    List<String> withCard = await context.waitForCard(readers).value;
+
+    print('Connecting to card in ${withCard.first}');
+    PcscCard card = await context.connect(
+      withCard.first,
+      ShareMode.shared,
+      Protocol.any,
+    );
+
+    print('Transmitting');
+    Uint8List resp = await card.transmit(
+      Uint8List.fromList([0x00, 0xa4, 0x04, 0x00, 1, 2, 3]),
+    );
+    int status = (resp[0] << 8) + resp[1];
+    print('Status: 0x${status.toRadixString(16)}');
+
+    await card.disconnect(Disposition.resetCard);
+  } finally {
+    await context.release();
+  }
+}

f30f1b343cb98b7e52e81948bdddf1e98f313f11
Jakub Janků <janku.jakub.jj@gmail.com>
2023-11-10 10:00:00 +0100

    explicitly list exported names

--- lib/dart_pcsc.dart
+++ lib/dart_pcsc.dart
@@ -1,3 +1,3 @@
-export 'src/constants.dart';
+export 'src/constants.dart' show Scope, Protocol, ShareMode, Disposition;
 export 'src/context.dart' show Context, Card;
 export 'src/exceptions.dart' show CardException;
